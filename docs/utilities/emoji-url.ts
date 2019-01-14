51b3bd1a88c9f744ca69d89eebe4b60781f8f9c7
Andy Mockler <asmockler@gmail.com>
2019-01-14 09:35:58 -0500

    Update docs URLs for new URL schema

--- docs/utilities/emoji-url.ts
+++ docs/utilities/emoji-url.ts
@@ -1,7 +1,7 @@
 export const SERVICE_PATH =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:9000/emoji'
-    : 'https://emoji.veryfunparty.com';
+    : 'https://emoji.veryfunparty.com/i';
 
 export function emojiUrl(name: string) {
   return `${SERVICE_PATH}/${name}`;
